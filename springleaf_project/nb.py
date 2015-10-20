53852d8d0aad4a8a886a25d4eca381fd0072b732
Yiji Zhang <yiji21@gmail.com>
2015-10-20 18:21:24 -0500

    1. Added project description. 2. Added the code (nb.py) for the preliminary experiment: 1) to run the code, please put the data file, train.csv, in the same directory as of the code (because the file name and path are all hard-coded for now). 2) classifier: Multinomial Naive Bayes provided by sklearn.naive_bayes library, 3) data pre-processing: the general idea is to convert all values into non-negative numbers (int, long, or float). For categorical string-type value, each possible value is mapped to a distinct integer. For missing data, they are filled simply with 0. For negative numbers, they are converted to their absolute values.

---
+++ springleaf_project/nb.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 09:38:59 2015
+
+Referece:
+1) multinomial Naive Bayes classifier
+http://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.MultinomialNB.html
+
+
+@author: Yiji
+"""
+
+import csv
+import random
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+#==============================================================================
+# Global variables
+#==============================================================================
+INFO = '[INFO]'
+DEBUG = '[DEBUG]'
+
+
+def loadCsv(fname):
+    lines = csv.reader(open(fname, "rU"),delimiter=",")
+    data = list(lines)
+    dataarray = np.asarray(data)[1:,1:] # skip the first line (the feature name), and the first column (IDs)
+    
+    return dataarray
+    
+def preprocessData(data):
+    featureSize = int(data.shape[1])  # total number of features
+    newData = np.zeros((len(data),featureSize))
+#    print newData
+    
+    for i in range(featureSize): # for values of each feature. 
+        col = i - 1
+        colArray = data[:,col]
+        d = dict() # to hold {string,intID} map
+        row = 0
+#==============================================================================
+#        numID:
+#        used to map an int identifier to each distinct string value; 
+#        also used for missing data, but this is not well designed because the placeholder for missing data 
+#        should not be the same as any existing values of the same feature. We'll deal with it later.        
+#==============================================================================
+        numId = 0
+        for element in colArray:
+            if isInt(element):
+                element = int(element)
+                # for negative data:                 
+                if element < 0:
+                    element = abs(element)
+                newData[row,col] = element
+            elif isLong(element):
+                element = long(element)
+                newData[row,col] = element
+            elif isFloat(element):
+                element = float(element)
+                newData[row,col] = element
+            else: #if the value is string, then replace string with int value.
+#                print element
+#                print d     
+#                print ''
+                if element not in d:
+                    d[element]=numId
+                    numId += 1
+                element = d[element]
+                newData[row,col] = element
+            row += 1
+                
+    return newData    
+#==============================================================================
+# take an array, iterate through it and count the occurrences
+# return a dictionary of {value, Count} 
+#==============================================================================
+def countOccurrences(array):
+    d = dict()
+    for key in array:
+        # check and/or convert value type of key
+        if isInt(key):
+            key = int(key)
+        elif isLong(key):
+            key = long(key)
+        elif isFloat(key):
+            key = float(key)
+#        else:
+#            #string
+        
+        # update count map for each key
+        if key in d:
+            d[key]+=1
+        else:
+            d[key]=1
+#    print d
+    return d
+    # if the key is a string
+    
+
+def separateByClass(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if (vector[-1] not in separated):
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+    return separated
+
+
+#==============================================================================
+# Helper functions to check variable type
+#==============================================================================   
+def isInt(valueStr):    
+    try: 
+        int(valueStr)
+        return True
+    except ValueError:
+        return False
+
+def isLong(valueStr):
+    try: 
+        long (valueStr)
+        return True
+    except ValueError:
+        return False
+
+def isFloat(valueStr):
+    try:
+        float(valueStr)
+        return True
+    except ValueError:
+        return False
+
+
+def splitDataset(dataset, splitRatio):
+    trainSize = int(len(dataset) * splitRatio)
+    trainSet = []
+    copy = list(dataset)
+    while len(trainSet) < trainSize:
+        index = random.randrange(len(copy))
+        trainSet.append(copy.pop(index))
+    return [trainSet, copy]
+
+    
+def main():
+#    trainFile = 'train.csv'    
+#    testFile = 'test.csv'
+    dataFile = 'train.csv'
+
+
+#    print '%s Loading and pre-processing training data...' % INFO 
+#    trainData = loadCsv(trainFile)
+##    print '%s Original training data:' % DEBUG
+##    print trainData
+#    trainData = preprocessData(trainData)
+##    print '%s Pre-processed training data:' % DEBUG
+##    print trainData
+#    
+#    print '%s Loading and pre-processing testing data...' % INFO 
+#    testData = loadCsv(testFile)
+#    testData = preprocessData(testData)
+##    print testData
+#    
+
+    
+    print '%s Loading and pre-processing data...' % INFO 
+    data = loadCsv(dataFile)
+#    print '%s Original training data:' % DEBUG
+#    print data
+    data = preprocessData(data)
+#    print '%s Pre-processed training data:' % DEBUG
+#    print data
+
+    # Split the data into training and testing    
+    splitRatio = 0.67
+    train, test = splitDataset(data, splitRatio)
+    train = np.asarray(train)
+    test = np.asarray(test)
+#    print train
+#    print test
+    
+    trainX = train[:,:-1]
+    trainy = train[:,-1]
+    
+    testX = test[:,:-1]
+    testy = test[:,-1]
+    
+    print '%s Fitting Naive Bayes classifier...' % INFO 
+    clf = MultinomialNB().fit(trainX, trainy)
+    print '%s Making predictions...' % INFO 
+    # for testing
+#    prediction = clf.predict(trainX)
+    prediction = clf.predict(testX)
+    print '[RESULT] the prediction is: '    
+    print prediction
+    print '%s Calcualting prediction accuracy...' % INFO 
+    # for testing
+#    accuracy = np.mean(prediction == trainy) 
+    accuracy = np.mean(prediction == testy)  
+    print '[RESULT] accuracy = %f' % accuracy
+#==============================================================================
+#     the code below is for the purpose of manually creating Naive Bayes clf
+#==============================================================================
+#    featureSize = int(trainData.shape[1])  # total number of features
+#    
+#    occArray = [] # each element is a dictionary of {value,count} for each feature
+#    for i in range(featureSize):
+#        col = i - 1
+#        colArray = trainData[:,col]
+#        occArray.append(countOccurrences(colArray))
+#        #print trainData[:,col]
+#    print occArray
+#    separated = separateByClass(trainData)
+#    print('Separated instances: {0}').format(separated)
+    
+#    print dataarray[:,0] #access the first column
+main()
+
+
+#==============================================================================
+# Testing code
+#==============================================================================
+#print isInt('-100')
+#print isFloat('-3.14567')
+
+
+
