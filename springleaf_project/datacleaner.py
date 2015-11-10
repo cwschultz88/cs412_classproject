04432402072bfafa4641d057de223c449cde6f9d
Chris Schultz <chris@10-8-234-113.vlan877.client.wireless.uic.edu>
2015-11-10 11:45:07 -0600

    added data cleaner interface

---
+++ springleaf_project/datacleaner.py
@@ -0,0 +1,32 @@
+import ABC
+
+
+class DataCleaner(Object):
+    '''
+    Interface for data cleaner classes
+
+    Data cleaner classes must extend this class to ensure a proper
+    interface if used.
+
+    Concrete implementations of this class must implement the
+    static method clean()
+
+    E.x.
+    class DataCleanerChris(Data):
+        ...
+        @staticmethod
+        def preprocess():
+            ...
+            return numpy_array
+    '''
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def clean(numpy_feature_array):
+        '''
+        Concrete implementations must implement this as a @staticmethod
+
+        Implementations need to accept a raw numpy_feature_array
+        and return a numpy_array that has been cleaned.
+        '''
+        pass
